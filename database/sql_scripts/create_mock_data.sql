9c9f60d3681d4796fb9629f0eaceda2418a8770b
Jacob Schirmer <Jacobmschirmer01@gmail.com>
2026-03-03 10:08:34 -0900

    remove template component

--- database/sql_scripts/create_mock_data.sql
+++ database/sql_scripts/create_mock_data.sql
@@ -1,6 +1,5 @@
 -- Insert mock vulnerabilities data
 INSERT INTO vulnerabilities (id, title, name, description, link) VALUES
-    ('template', 'Template Vulnerability', 'Template Vulnerability', 'This is a template vulnerability for testing purposes.', 'https://cwe.mitre.org/'),
     ('Improper-Neutralization-of-Input-During-Web-Page-Generation', 'Cross-site Scripting', 'Improper Neutralization of Input During Web Page Generation', 'Inject malicious scripts into web pages viewed by other users.', 'https://cwe.mitre.org/data/definitions/79.html'),
     ('Improper-Neutralization-of-Special-Elements-used-in-an-SQL-Command', 'SQL Injection', 'Improper Neutralization of Special Elements used in an SQL Command', 'Manipulate database queries to access or modify unauthorized data.', 'https://cwe.mitre.org/data/definitions/89.html'),
     ('Cross-Site-Request-Forgery', 'Cross-Site Request Forgery', 'Cross-Site Request Forgery', 'Force users to execute unwanted actions on authenticated websites.', 'https://cwe.mitre.org/data/definitions/352.html'),
