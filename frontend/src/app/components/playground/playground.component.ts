d549c8720917cb551243e941846175efb2a43a9b
anikahedden@gmail.com <anikahedden@gmail.com>
2026-03-02 18:22:21 -0900

    good implementation done

--- frontend/src/app/components/playground/playground.component.ts
+++ frontend/src/app/components/playground/playground.component.ts
@@ -30,25 +30,10 @@ export class PlaygroundComponent {
   }
  /* ---------- Missing Authorization ---------- */
 
-  currentUser = {
-    id: 2 // change to 1–4 to test
-  };
-
-  posts = [
-    { userId: 1, content: 'Post written by User 1' },
-    { userId: 2, content: 'Post written by User 2' },
-    { userId: 3, content: 'Post written by User 3' },
-    { userId: 4, content: 'Post written by User 4' }
-  ];
-
-  canEdit(post: any): boolean {
-    return post.userId === this.currentUser.id;
-  }
-
-  savePost(post: any) {
-    if (!this.canEdit(post)) return;
-    // simulate persistence
-    console.log('Saved post:', post);
+ getStoredValue2() {
+    this.savedValue$ = this.goodService.getXSSValue('user-input').pipe(
+      map(response => response.data.value)
+    )
   }
   
 }
